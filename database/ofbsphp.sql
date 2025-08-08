7a81fc46fbbbc9bb5711f3f8c312facef0057cf6
Muhammad Zohair <mohammadzohair41@gmail.com>
2025-08-08 13:42:39 +0500

    Add files via upload

---
+++ database/ofbsphp.sql
@@ -0,0 +1,437 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2025 at 04:37 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ofbsphp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_uname` varchar(20) NOT NULL,
+  `admin_email` varchar(50) NOT NULL,
+  `admin_pwd` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_uname`, `admin_email`, `admin_pwd`) VALUES
+(1, 'Annan', 'annan@mail.com', '$2a$12$ZV/4mPR9hsjz9kafDlXOGeS9fbSloW2U.JCSdV9MFIx.rUWJ1d86i');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `airline`
+--
+
+CREATE TABLE `airline` (
+  `airline_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `seats` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `airline`
+--
+
+INSERT INTO `airline` (`airline_id`, `name`, `seats`) VALUES
+(21, 'PAKAIRLINE1', 21),
+(22, 'PAKAIRLINE2', 46),
+(23, 'PAKAIRLINE3', 17),
+(24, 'PAKAIRLINE4', 7),
+(25, 'PAKAIRLINE5', 84),
+(26, 'PAKAIRLINE6', 55);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `city` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`city`) VALUES
+('San Jose'),
+('Chicago'),
+('Olisphis'),
+('Shiburn'),
+('Weling'),
+('Chiby'),
+('Odonhull'),
+('Hegan'),
+('Oriaridge'),
+('Flerough'),
+('Yleigh'),
+('Oyladnard'),
+('Trerdence'),
+('Zhotrora'),
+('Otiginia'),
+('Plueyby'),
+('Vrexledo'),
+('Ariosey');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feed_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `q1` varchar(250) NOT NULL,
+  `q2` varchar(20) NOT NULL,
+  `q3` varchar(250) NOT NULL,
+  `rate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feed_id`, `email`, `q1`, `q2`, `q3`, `rate`) VALUES
+(1, 'mohammadzohair41@gmail.com', 'zxjcgvBSc ', 'Social Media', '', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `flight`
+--
+
+CREATE TABLE `flight` (
+  `flight_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `arrivale` datetime NOT NULL,
+  `departure` datetime NOT NULL,
+  `Destination` varchar(20) NOT NULL,
+  `source` varchar(20) NOT NULL,
+  `airline` varchar(20) NOT NULL,
+  `Seats` varchar(110) NOT NULL,
+  `duration` varchar(20) NOT NULL,
+  `Price` int(11) NOT NULL,
+  `status` varchar(6) DEFAULT NULL,
+  `issue` varchar(50) DEFAULT NULL,
+  `last_seat` varchar(5) DEFAULT '',
+  `bus_seats` int(11) DEFAULT 20,
+  `last_bus_seat` varchar(5) DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `flight`
+--
+
+INSERT INTO `flight` (`flight_id`, `admin_id`, `arrivale`, `departure`, `Destination`, `source`, `airline`, `Seats`, `duration`, `Price`, `status`, `issue`, `last_seat`, `bus_seats`, `last_bus_seat`) VALUES
+(1, 1, '2022-06-30 10:03:00', '2022-06-30 09:01:00', 'Chicago', 'San', 'Core Airways', '63', '1', 175, '', '', '21B', 20, ''),
+(2, 1, '2022-07-05 11:15:00', '2022-07-05 10:05:00', 'Shiburn', 'Olisphis', 'Core Airways', '61', '1', 185, 'arr', '', '21D', 20, ''),
+(3, 1, '2022-07-05 12:13:00', '2022-07-05 10:13:00', 'Weling', 'Olisphis', 'Spark Airways', '123', '2', 205, 'arr', '', '21B', 20, ''),
+(8, 1, '2022-07-06 00:55:00', '2022-07-05 17:00:00', 'Oyladnard', 'Odonhull', 'Homelander Airways', '183', '7', 615, 'arr', '', '21B', 20, ''),
+(20, 1, '2022-07-05 23:58:00', '2022-07-05 22:14:00', 'Zhotrora', 'Trerdence', 'Aero Airways', '208', '1', 0, 'dep', '', '21B', 20, ''),
+(23, 1, '2025-06-13 06:30:00', '2025-06-12 06:30:00', 'Chicago', 'San', 'PAKAIRLINE1', '21', '12', 1200, '', '', '', 20, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passenger_profile`
+--
+
+CREATE TABLE `passenger_profile` (
+  `passenger_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `mobile` varchar(110) NOT NULL,
+  `dob` datetime NOT NULL,
+  `f_name` varchar(20) DEFAULT NULL,
+  `m_name` varchar(20) DEFAULT NULL,
+  `l_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `passenger_profile`
+--
+
+INSERT INTO `passenger_profile` (`passenger_id`, `user_id`, `flight_id`, `mobile`, `dob`, `f_name`, `m_name`, `l_name`) VALUES
+(3, 3, 2, '2147483647', '1994-06-21 00:00:00', 'Andre', 'J', 'Atkins'),
+(4, 4, 2, '2147483647', '1995-05-16 00:00:00', 'James', 'K', 'Harbuck'),
+(5, 2, 8, '7854444411', '1995-02-13 00:00:00', 'Henry', 'l', 'Stuart'),
+(6, 2, 20, '7412585555', '1995-02-13 00:00:00', 'Henry', 'l', 'Stuart');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `card_no` varchar(16) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `expire_date` varchar(5) DEFAULT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`card_no`, `user_id`, `flight_id`, `expire_date`, `amount`) VALUES
+('1010555677851111', 4, 2, '10/26', 370),
+('1020445869651011', 2, 20, '12/25', 370),
+('1111888889897778', 2, 3, '12/25', 205),
+('1400565800004478', 2, 8, '12/25', 1230),
+('1458799990001450', 3, 2, '12/25', 185),
+('4204558500014587', 1, 1, '02/25', 350);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pwdreset`
+--
+
+CREATE TABLE `pwdreset` (
+  `pwd_reset_id` int(11) NOT NULL,
+  `pwd_reset_email` varchar(50) NOT NULL,
+  `pwd_reset_selector` varchar(80) NOT NULL,
+  `pwd_reset_token` varchar(120) NOT NULL,
+  `pwd_reset_expires` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `pwdreset`
+--
+
+INSERT INTO `pwdreset` (`pwd_reset_id`, `pwd_reset_email`, `pwd_reset_selector`, `pwd_reset_token`, `pwd_reset_expires`) VALUES
+(1, 'mohammadzohair41@gmail.com', '4a9ad923185fc138', '$2y$10$RyKEaiKa.natqJmXgegtKuctzvnYmN8c5CITZMMNYgs36Ucejwz6u', '1749651660');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `ticket_id` int(11) NOT NULL,
+  `passenger_id` int(11) NOT NULL,
+  `flight_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `seat_no` varchar(10) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `class` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`ticket_id`, `passenger_id`, `flight_id`, `user_id`, `seat_no`, `cost`, `class`) VALUES
+(4, 3, 2, 3, '21A', 185, 'E'),
+(6, 4, 2, 4, '21C', 370, 'E'),
+(8, 5, 8, 2, '21A', 1230, 'E'),
+(10, 6, 20, 2, '21A', 370, 'E');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
+(1, '', '', ''),
+(2, '', '', ''),
+(3, '', '', ''),
+(4, '', '', ''),
+(5, 'Muhammad Zohair', 'mohammadzohair41@gmail.com', '$2y$10$nD1atoVs1t2Ud4dZs/77uOlptFmYZ.Cvma6VzmOX8vMJIJnwe5fve');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `airline`
+--
+ALTER TABLE `airline`
+  ADD PRIMARY KEY (`airline_id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feed_id`);
+
+--
+-- Indexes for table `flight`
+--
+ALTER TABLE `flight`
+  ADD PRIMARY KEY (`flight_id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `passenger_profile`
+--
+ALTER TABLE `passenger_profile`
+  ADD PRIMARY KEY (`passenger_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `flight_id` (`flight_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`card_no`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `flight_id` (`flight_id`);
+
+--
+-- Indexes for table `pwdreset`
+--
+ALTER TABLE `pwdreset`
+  ADD PRIMARY KEY (`pwd_reset_id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`ticket_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `flight_id` (`flight_id`),
+  ADD KEY `passenger_id` (`passenger_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `airline`
+--
+ALTER TABLE `airline`
+  MODIFY `airline_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feed_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `flight`
+--
+ALTER TABLE `flight`
+  MODIFY `flight_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `passenger_profile`
+--
+ALTER TABLE `passenger_profile`
+  MODIFY `passenger_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pwdreset`
+--
+ALTER TABLE `pwdreset`
+  MODIFY `pwd_reset_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `flight`
+--
+ALTER TABLE `flight`
+  ADD CONSTRAINT `flight_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+
+--
+-- Constraints for table `passenger_profile`
+--
+ALTER TABLE `passenger_profile`
+  ADD CONSTRAINT `passenger_profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `passenger_profile_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`flight_id`) REFERENCES `flight` (`flight_id`),
+  ADD CONSTRAINT `ticket_ibfk_3` FOREIGN KEY (`passenger_id`) REFERENCES `passenger_profile` (`passenger_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
